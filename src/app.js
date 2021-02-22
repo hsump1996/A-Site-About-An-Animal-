058e5e05068c28e26955e20a4741391e7183bdd7
Sunpyo Hong <sh4023@nyu.edu>
2021-02-22 22:10:49 +0900

    update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 // app.js
+
+const App = require('./demo.js');
+const app = new App();
+
+app.get('/foo', function(req, res) {
+  res.send('foo');
+});
+
+app.get('/bar', function(req, res) {
+  res.send('<h1>bar</h1>');
+});
+
+app.listen(3000, '127.0.0.1');
--- src/webby.js
+++ src/webby.js
@@ -1,1 +1,31 @@
 // webby.js
+
+const net = require('net');
+
+
+const HTTP_STATUS_CODES = {
+    200: 'OK',
+    404: 'NOT FOUND',
+    500: 'SERVER ERROR'
+};
+
+const MIME_TYPES = {
+    jpg: 'image/jpeg',
+    jpeg: 'image/jpeg',
+    png: 'image/png',
+    html: 'text/html',
+    css: 'text/css',
+    txt: 'text/plain'
+};
+
+function getExtension(fileName) {
+
+    
+
+}
+
+
+
+
+
+module.exports = App;
